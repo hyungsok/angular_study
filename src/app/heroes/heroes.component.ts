c34030d927ed2894d877cd5ed3bf2c02a1c9bca3
kevin <kevin.lee@mangoplate.com>
2018-04-07 14:56:43 +0900

    hero 관련 처리 하면서 this에 대한 처리 확인

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,12 +7,16 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
 
+  hero = new Hero();
   constructor() {
+    // this 생략을 못함 HeroesComponent를 프레임단에서 생성해주지 못하기 때문
+    this.hero.id = 2;
+    this.hero.name = 'Windstorm';
   }
 
   ngOnInit() {
