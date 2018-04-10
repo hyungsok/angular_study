4f50ad66f2e304cfb97ad2ade178addec70a14d3
kevin <kevin.lee@mangoplate.com>
2018-04-10 21:33:03 +0900

    Message 컴포넌트 서비스 추가 적용

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,17 @@ import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Observable} from 'rxjs/Observable';
 import {of} from 'rxjs/observable/of';
+import {MessagesService} from './messages.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() {
+  constructor(private messageService: MessagesService) {
   }
 
   getHeroes(): Observable<Hero[]> {
+    // Todo: send the messages _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
